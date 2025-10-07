f7994d01acce5d25b78775fdcab6b50001d95bd6
naila saleem <zeeraplus69@gmail.com>
2025-10-07 07:55:01 -0700

    Added legal modals (Claims, Privacy, Terms) and updated styles/scripts

--- script.js
+++ script.js
@@ -148,3 +148,23 @@ document.addEventListener("DOMContentLoaded", () => {
             
             populateMarquee();
         });
+
+        // Modal term
+        function openModal(id) {
+  document.getElementById(id).style.display = 'flex';
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = 'none';
+}
+
+// Close modal if user clicks outside the box
+window.onclick = function(event) {
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+};
+
